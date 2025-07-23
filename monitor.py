f0913dfa4ef598888457aa9565b8582395aa693b
ethannjw <ethannjw@hotmail.com>
2025-07-23 23:02:27 +0800

    moved notifications.py

--- monitor.py
+++ monitor.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from scraper.main import KTMBShuttleScraper
 from utils.config import ScraperSettings, Direction, TimeSlot
-from notifications import create_notification_sender
+from notifications.notifications import create_notification_sender
 from utils.logging_config import setup_logging, get_logger
 from scraper.healthcheck import run_healthcheck_server
 
